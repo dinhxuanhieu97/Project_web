7236bbd794128edccf7d510bb24e04658a69e980
keyDev <dxhieu97@gmail.com>
2025-03-28 13:44:22 +0700

    removed and update file sql

--- addproduct.php
+++ addproduct.php
@@ -18,36 +18,41 @@ if(!empty($_POST['product_name']) && !empty($_POST['product_description']) && !e
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="http://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="http://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 </head>
+
 <body>
-    <div class="container">
-        <h1>Add product</h1>
-        <form action="addproduct.php" method="post">
-            <div class="mb-3">
-                <label for="product_name" class="form-label">Tên sản phẩm</label>
-                <input type="text" class="form-control" id="product_name" name="product_name">
-            </div>
-            <div class="mb-3">
-                <label for="product_description" class="form-label">Mô tả</label>
-                <input type="text" class="form-control" id="product_description" name="product_description">
-            </div>
-            <div class="mb-3">
-                <label for="product_price" class="form-label">Giá</label>
-                <input type="text" class="form-control" id="product_price" name="product_price">
-            </div>
-            <div class="mb-3">
-                <label for="product_photo" class="form-label">Hình</label>
-                <input type="text" class="form-control" id="product_photo" name="product_photo">
-            </div>
-            <button type="submit" class="btn btn-primary">Add</button>
-        </form>
-    </div>
+  <div class="container">
+    <h1>Add product</h1>
+    <form action="addproduct.php" method="post">
+      <div class="mb-3">
+        <label for="product_name" class="form-label">Tên sản phẩm</label>
+        <input type="text" class="form-control" id="product_name" name="product_name">
+      </div>
+      <div class="mb-3">
+        <label for="product_description" class="form-label">Mô tả</label>
+        <input type="text" class="form-control" id="product_description" name="product_description">
+      </div>
+      <div class="mb-3">
+        <label for="product_price" class="form-label">Giá</label>
+        <input type="text" class="form-control" id="product_price" name="product_price">
+      </div>
+      <div class="mb-3">
+        <label for="product_photo" class="form-label">Hình</label>
+        <input type="text" class="form-control" id="product_photo" name="product_photo">
+      </div>
+      <button type="submit" class="btn btn-primary">Add</button>
+    </form>
+
+  </div>
 </body>
+
 </html>
